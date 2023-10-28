71281a497e9105093199811faaa0eba91952d696
Priyambada Ray <117258378+priyambadaray2016@users.noreply.github.com>
2023-10-28 22:30:46 +0530

    protofolio

---
+++ main.js
@@ -0,0 +1,30 @@
+var typed = new typed(".text",{
+    String:["Fontend Devloper","youtuber","web Devloper"],
+    typespeed:100,
+    backspeed:100,
+    backDealy:1000,
+    loop:true,
+})
+function f1(){
+    document.write("ISR. ENG. SERVICE & APPLICATION || 6 SIGMA BB ||<br> B.TECH Manufacturing || DIPLOMA TOOL & DIE MAKING || LIQUID SILICONE RUBBER MOULDING SPECIALIST || <br>NEW PRODUCT DEVELOPMENTSR. ENG. SERVICE & APPLICATION || 6 SIGMA BB || B.TECH Manufacturing || DIPLOMA TOOL & DIE MAKING || LIQUID SILICONE RUBBER MOULDING SPECIALIST || NEW PRODUCT DEVELOPMENT");
+    
+}
+function f2(){
+    document.write("A results-oriented Senior Engineer with over 8 years of hands-on experience in service,<br> application, troubleshooting, and new product development within the plastics industry. Expertise in Liquid Silicone Rubber (LSR) molding, <br>green field project execution, and optimizing production processes. Proven track record of delivering innovative solutions,<br> independently managing projects, and supporting clients nationwide.");
+    
+}
+function f3(){ 
+     document.write(
+    "Industry Knowledge sr engineer service and application at ELECTRONICA PLASTIC MACHINES LIMITEDsr engineer service and application at ELECTRONICA PLASTIC MACHINES LIMITED<br>PowerPointPowerPointendorsement1 endorsement Microsoft OfficeMicrosoft OfficeCustomer ServiceCustomer Service Microsoft ExcelMicrosoft ExcelPassed LinkedIn Skill AssessmentPassed LinkedIn Skill Assessment ManagementManagementLeadershipLeadership liquid silicone rubber processingliquid silicone rubber processing Production Executive(IMD & NPD) at Chemco GroupProduction Executive(IMD & NPD) at Chemco Group<br>Project ManagementProject Management Microsoft WordMicrosoft WordStrategic PlanningStrategic Planning ResearchResearchAutoCADAutoCAD CATIACATIA NX UnigraphicsNX UnigraphicsPTC CreoPTC Creo Service EngineeringInjection MoldingInjection Molding")
+    
+    
+}
+function f4(){
+    document.write("it's swapnajeet protfolio");
+    
+}
+function f5(){
+    document.write("my contact no-7387184009 <br> email-swapnajeet.mohanta@gmail.com");
+}
+    
+    
